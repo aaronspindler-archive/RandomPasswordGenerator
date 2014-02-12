13b705df28e55130e6dba674c8d4977e0dae900c
xNovax <nova1104@live.com>
2014-02-11 22:36:24 -0500

    Added all of the nessassary files.

---
+++ RandomPasswordGenerator/optionsForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace RandomPasswordGenerator
+{
+    partial class optionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.specialCharLabel = new System.Windows.Forms.Label();
+            this.numbersLabel = new System.Windows.Forms.Label();
+            this.lowercaseLabel = new System.Windows.Forms.Label();
+            this.optionsTitle = new System.Windows.Forms.Label();
+            this.uppercaseLabel = new System.Windows.Forms.Label();
+            this.uppercaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.lowercaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.numbersCheckBox = new System.Windows.Forms.CheckBox();
+            this.specialCharCheckBox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.specialCharLabel, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.numbersLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lowercaseLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.optionsTitle, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.uppercaseLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.uppercaseCheckBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lowercaseCheckBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.numbersCheckBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.specialCharCheckBox, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(383, 144);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // specialCharLabel
+            // 
+            this.specialCharLabel.AutoSize = true;
+            this.specialCharLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.specialCharLabel.Location = new System.Drawing.Point(4, 115);
+            this.specialCharLabel.Name = "specialCharLabel";
+            this.specialCharLabel.Size = new System.Drawing.Size(184, 28);
+            this.specialCharLabel.TabIndex = 7;
+            this.specialCharLabel.Text = "Special Characters";
+            this.specialCharLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numbersLabel
+            // 
+            this.numbersLabel.AutoSize = true;
+            this.numbersLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numbersLabel.Location = new System.Drawing.Point(4, 89);
+            this.numbersLabel.Name = "numbersLabel";
+            this.numbersLabel.Size = new System.Drawing.Size(184, 25);
+            this.numbersLabel.TabIndex = 5;
+            this.numbersLabel.Text = "Numbers";
+            this.numbersLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lowercaseLabel
+            // 
+            this.lowercaseLabel.AutoSize = true;
+            this.lowercaseLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lowercaseLabel.Location = new System.Drawing.Point(4, 63);
+            this.lowercaseLabel.Name = "lowercaseLabel";
+            this.lowercaseLabel.Size = new System.Drawing.Size(184, 25);
+            this.lowercaseLabel.TabIndex = 3;
+            this.lowercaseLabel.Text = "Lower Case";
+            this.lowercaseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // optionsTitle
+            // 
+            this.optionsTitle.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.optionsTitle, 2);
+            this.optionsTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.optionsTitle.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsTitle.Location = new System.Drawing.Point(4, 1);
+            this.optionsTitle.Name = "optionsTitle";
+            this.optionsTitle.Size = new System.Drawing.Size(375, 35);
+            this.optionsTitle.TabIndex = 0;
+            this.optionsTitle.Text = "Options";
+            this.optionsTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uppercaseLabel
+            // 
+            this.uppercaseLabel.AutoSize = true;
+            this.uppercaseLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uppercaseLabel.Location = new System.Drawing.Point(4, 37);
+            this.uppercaseLabel.Name = "uppercaseLabel";
+            this.uppercaseLabel.Size = new System.Drawing.Size(184, 25);
+            this.uppercaseLabel.TabIndex = 1;
+            this.uppercaseLabel.Text = "Upper Case";
+            this.uppercaseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uppercaseCheckBox
+            // 
+            this.uppercaseCheckBox.AutoSize = true;
+            this.uppercaseCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uppercaseCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uppercaseCheckBox.Location = new System.Drawing.Point(195, 40);
+            this.uppercaseCheckBox.Name = "uppercaseCheckBox";
+            this.uppercaseCheckBox.Size = new System.Drawing.Size(184, 19);
+            this.uppercaseCheckBox.TabIndex = 2;
+            this.uppercaseCheckBox.UseVisualStyleBackColor = true;
+            this.uppercaseCheckBox.CheckedChanged += new System.EventHandler(this.uppercaseCheckBox_CheckedChanged);
+            // 
+            // lowercaseCheckBox
+            // 
+            this.lowercaseCheckBox.AutoSize = true;
+            this.lowercaseCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lowercaseCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lowercaseCheckBox.Location = new System.Drawing.Point(195, 66);
+            this.lowercaseCheckBox.Name = "lowercaseCheckBox";
+            this.lowercaseCheckBox.Size = new System.Drawing.Size(184, 19);
+            this.lowercaseCheckBox.TabIndex = 4;
+            this.lowercaseCheckBox.UseVisualStyleBackColor = true;
+            this.lowercaseCheckBox.CheckedChanged += new System.EventHandler(this.lowercaseCheckBox_CheckedChanged);
+            // 
+            // numbersCheckBox
+            // 
+            this.numbersCheckBox.AutoSize = true;
+            this.numbersCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.numbersCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numbersCheckBox.Location = new System.Drawing.Point(195, 92);
+            this.numbersCheckBox.Name = "numbersCheckBox";
+            this.numbersCheckBox.Size = new System.Drawing.Size(184, 19);
+            this.numbersCheckBox.TabIndex = 6;
+            this.numbersCheckBox.UseVisualStyleBackColor = true;
+            this.numbersCheckBox.CheckedChanged += new System.EventHandler(this.numbersCheckBox_CheckedChanged);
+            // 
+            // specialCharCheckBox
+            // 
+            this.specialCharCheckBox.AutoSize = true;
+            this.specialCharCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.specialCharCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.specialCharCheckBox.Location = new System.Drawing.Point(195, 118);
+            this.specialCharCheckBox.Name = "specialCharCheckBox";
+            this.specialCharCheckBox.Size = new System.Drawing.Size(184, 22);
+            this.specialCharCheckBox.TabIndex = 8;
+            this.specialCharCheckBox.UseVisualStyleBackColor = true;
+            this.specialCharCheckBox.CheckedChanged += new System.EventHandler(this.specialCharCheckBox_CheckedChanged);
+            // 
+            // optionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 144);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "optionsForm";
+            this.Text = "Options";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.optionsForm_FormClosing);
+            this.Load += new System.EventHandler(this.optionsForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label optionsTitle;
+        private System.Windows.Forms.Label uppercaseLabel;
+        private System.Windows.Forms.Label specialCharLabel;
+        private System.Windows.Forms.Label numbersLabel;
+        private System.Windows.Forms.Label lowercaseLabel;
+        private System.Windows.Forms.CheckBox uppercaseCheckBox;
+        private System.Windows.Forms.CheckBox lowercaseCheckBox;
+        private System.Windows.Forms.CheckBox numbersCheckBox;
+        private System.Windows.Forms.CheckBox specialCharCheckBox;
+    }
+}
---
+++ RandomPasswordGenerator/optionsForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPasswordGenerator
+{
+    public partial class optionsForm : Form
+    {
+        //Variable Declaration
+        Boolean uppercase;
+        Boolean lowercase;
+        Boolean numbers;
+        Boolean specialChar;
+
+        public optionsForm()
+        {
+            InitializeComponent();
+        }
+
+        //Checks to see if the box's state has been changed
+        private void uppercaseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (uppercaseCheckBox.Checked == true)
+            {
+                Properties.Settings.Default.uppercase = true;
+                uppercase = true;
+            }
+            else
+            {
+                Properties.Settings.Default.uppercase = false;
+                uppercase = false;
+            }
+        }
+
+        //Checks to see if the box's state has been changed
+        private void lowercaseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lowercaseCheckBox.Checked == true)
+            {
+                Properties.Settings.Default.lowercase = true;
+                lowercase = true;
+            }
+            else
+            {
+                Properties.Settings.Default.lowercase = false;
+                lowercase = false;
+            }
+        }
+
+        //Checks to see if the box's state has been changed
+        private void numbersCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (numbersCheckBox.Checked == true)
+            {
+                Properties.Settings.Default.numbers = true;
+                numbers = true;
+            }
+            else
+            {
+                Properties.Settings.Default.numbers = false;
+                numbers = false;
+            }
+        }
+
+        //Checks to see if the box's state has been changed
+        private void specialCharCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (specialCharCheckBox.Checked == true)
+            {
+                Properties.Settings.Default.specialChar = true;
+                specialChar = true;
+            }
+            else
+            {
+                Properties.Settings.Default.specialChar = false;
+                specialChar = false;
+            }
+        }
+
+        //Checks the settings to see what the users settings are. Then it changes which check boxes are checked.
+        private void optionsForm_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.uppercase == true)
+            {
+                uppercaseCheckBox.Checked = true;
+                uppercase = true;
+            }
+            else
+            {
+                uppercaseCheckBox.Checked = false;
+                uppercase = false;
+            }
+
+            if (Properties.Settings.Default.lowercase == true)
+            {
+                lowercaseCheckBox.Checked = true;
+                lowercase = true;
+            }
+            else
+            {
+                lowercaseCheckBox.Checked = false;
+                lowercase = false;
+            }
+
+            if (Properties.Settings.Default.numbers == true)
+            {
+                numbersCheckBox.Checked = true;
+                numbers = true;
+            }
+            else
+            {
+                numbersCheckBox.Checked = false;
+                numbers = false;
+            }
+
+            if (Properties.Settings.Default.specialChar == true)
+            {
+                specialCharCheckBox.Checked = true;
+                specialChar = true;
+            }
+            else
+            {
+                specialCharCheckBox.Checked = false;
+                specialChar = false;
+            }
+        }
+
+        private void optionsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.uppercase = uppercase;
+            Properties.Settings.Default.lowercase = lowercase;
+            Properties.Settings.Default.numbers = numbers;
+            Properties.Settings.Default.specialChar = specialChar;
+        }
+
+    }
+}
